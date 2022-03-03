1c70d245954aab42a90412952534f5e213c78f48
Esteban <96262806+Castelblanco@users.noreply.github.com>
2022-03-03 14:53:10 -0500

    Update clima.vue

--- src/components/clima.vue
+++ src/components/clima.vue
@@ -38,7 +38,7 @@
         },
         methods: {
             async pedirTiempo(ct){
-                try {
+                
                     let respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ct}&units=metric&lang=es&appid=631b7ccc8feaac7191f5011c66d1e376`),
                         respuestaImg = await fetch(`http://api.weatherstack.com/current?access_key=8ab8bf8498c9cdc9cecb3cd0a9c4fe2c&query=${ct}`),
                         data = await respuesta.json(),
@@ -51,7 +51,6 @@
                     this.vnt = data.wind.speed;
                     this.error = "";
                     this.dprtmnt = "";
-                }catch(e){this.error = "La ciudad que buscas no esta registrada";}
 
             }
         }
