e61797c58f3e0736c0b6b95c9908bd6fed4e63d1
Castelblanco <ejcastelg@gmail.com>
2022-03-02 19:35:05 -0500

    Castel-Clima

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="intro">
+      <h1>Castel-Clima</h1>
+      <p>Bienvenido a Castel-Clima, esta pequeña App te mostrara el clima del hoy gracias a la API de <b>OpenWeather</b> y <b>WeatherStack</b>, solo escribe de forma correcta la ciudad de donde quieres conocer el clima y listo.</p>
+    </div>
+    <clima/>
+    <footer>Esteban Javier Castelblanco García</footer>
+  </div>
+</template>
+
+<script>
+  import clima from "./components/clima.vue";
+  export default {
+    name: 'App',
+    components: {
+      clima
+    }
+  }
+</script>
+
+<style>
+  body{
+    width: 100vw;
+    background-color: #333;
+    color: #fff;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
+
+  .intro{
+    width: 80%;
+    margin: 60px auto;
+  }
+
+</style>
---
+++ src/components/clima.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="clima">
+    <div class="dprtmnt">
+        <div>
+            <input class="depart"  type="text" v-model="dprtmnt" v-on:keyup.enter="dprtmnt.length >2 ?pedirTiempo(dprtmnt) :error = 'La ciudad que buscas no esta registrada'">
+            <button :class="[{'btn-ctv': dprtmnt.length > 2},'btn-nctv']"  v-on:click.enter="dprtmnt.length >2 ?pedirTiempo(dprtmnt) :error = 'La ciudad que buscas no esta registrada'">Chequear</button>
+        </div>
+        <b>{{error}}</b>
+    </div>
+    <div :class="[{'infoNo':ct == ''},'info']">
+        <div class="desc">
+            <b>{{ct}}</b>
+            <img ref="img" alt="Imagen del clima">
+            <p>{{desc}}</p>
+        </div>
+        <div class="datos">
+            <p>Temperatira: {{temp}}°C</p>
+            <p>Presion: {{prsn}} hPa</p>
+            <p>Viento: {{vnt}} m/s</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                dprtmnt: "",
+                error: "",
+                ct: "",
+                img: "",
+                desc: "",
+                temp: "",
+                prsn: "",
+                vnt: ""
+            }
+        },
+        methods: {
+            async pedirTiempo(ct){
+                try{
+                   let respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ct}&units=metric&lang=es&appid=631b7ccc8feaac7191f5011c66d1e376`),
+                   //respuestaImg = await fetch(`http://api.weatherstack.com/current?access_key=8ab8bf8498c9cdc9cecb3cd0a9c4fe2c&query=${ct}`),
+                   data = await respuesta.json();
+                   //dataImg = await respuestaImg.json();
+                //this.$refs.img.setAttribute("src", dataImg.current.weather_icons[0]);
+                
+                this.ct = data.name;
+                this.desc = data.weather[0].description;
+                this.temp = Math.round(data.main.temp);
+                this.prsn = data.main.pressure;
+                this.vnt = data.wind.speed;
+                this.error = "";
+                }catch(e) {
+                    this.error = "La ciudad que buscas no esta registrada";
+                }
+
+            }
+        }
+    }
+</script>
+
+<style>
+    img{
+        display: block;
+        margin: 10px;
+    }
+
+    button, input{
+        border: none;
+        outline: none;
+    }
+
+    .clima{
+        width: 70%;
+        margin: 10px auto;
+    }
+
+    .depart{
+        height: 23px;
+        border-radius: 5px;
+        padding: 0 2px;
+        transition: .9s;
+    }
+
+    .depart:hover{
+        background-color: #eee;
+    }
+
+    .dprtmnt{
+        display: block;
+    }
+
+    .dprtmnt > div{
+        margin-bottom: 10px;
+    }
+
+    .dprtmnt > b{
+        color: #e01;
+    }
+
+    .btn-nctv{
+        height: 23px;
+        border-radius: 5px;
+    }
+
+    .btn-ctv{
+        background-color: #a0e;
+        height: 23px;
+        border-radius: 5px;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    .info{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 70%;
+        height: fit-content;
+        margin: 30px auto;
+        padding: 10px 0;
+        box-shadow: 5px 4px 8px #000;
+    }
+
+    .infoNo{
+        display: none;
+    }
+
+    .desc{
+        display: block;
+    }
+
+    @media screen and (max-width:680px) {
+        .info{
+            display: block;
+        }
+
+        .desc{
+            margin: 20px 0;
+        }
+
+        .datos{
+            margin-top: 40px;
+        }
+    }
+
+</style>
